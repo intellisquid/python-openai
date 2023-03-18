2a47c523188f88dcda17acbe66e77a62325266e7
intellisquid <wshendricks@gmail.com>
2023-03-18 12:12:19 -0500

    Prompt
    Updated for custom prompt.

--- chatbot.py
+++ chatbot.py
@@ -2,12 +2,13 @@ import openai
 
 openai.api_key = "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"
 
+prompt = input("Your prompt: ")
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
             {"role": "system", "content": "You are a chatbot"},
-            {"role": "user", "content": "Why should DevOps engineer learn kubernetes?"},
+            {"role": "user", "content": prompt},
         ]
 )
 
